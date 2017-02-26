528909da59d964f46dc1437b2247e9fc07b0fde5
Michał Gumiela <michal.gumiela@gmail.com>
2017-02-26 20:10:22 +0100

    Degrees changed to microsteps of motors

--- TestStand.py
+++ TestStand.py
@@ -8,17 +8,21 @@ if MOCK is True:
             self.com = com
 
         def write(self, data):
-            print(self.com + ": " + data + "\r")
+            print(self.com + ": " + str(data) + "\r")
 
         def readline(self):
             return self.com + ": " + "read data!" + "\r"
+
+        def flushInput(self):
+            return "ok"
 else:
     import serial
 
 
 class Stand:
-    def __init__(self, com_stand):
+    def __init__(self, com_stand, verbose=False):
         self.com_stand = com_stand
+        self.verbose = verbose
 
         command_file = open('commands.txt', 'r')
 
@@ -38,16 +42,23 @@ class Stand:
         self.com_stand.write(text.encode())
         time.sleep(0.1)
 
+    def degreesToMicroSteps(self, x):
+        return x/((1.8/32.0)/20.0)
+
+    def microstepsToDegrees(self, x):
+        return x*((1.8/32.0)/20.0)
+
     def goToAPosition(self, x, y):
-        text = self.commands[2] + " " + str(x*10000) + " " + str(y*10000) + self.commands[3] + "\r"
-        self.com_stand.write(text.encode())
+        text = self.commands[2] + " " + str(x) + " " + str(y) + self.commands[3] + "\r"
         self.com_stand.flushInput()
+        self.com_stand.write(text.encode())
         cnt = 0
         while True:
             cnt += 1
             res = self.com_stand.readline().strip()
-            print(res.strip())
-            if cnt > 5:
+            if self.verbose:
+                print(res.strip())
+            if cnt > 500:
                 print("Cannot find ACK from the stand \r")
                 time.sleep(5)
                 self.goToAPosition(self, x, y)
--- main.py
+++ main.py
@@ -3,37 +3,51 @@ import time
 from SunS import SunS
 from TestStand import *
 
-MOCK = False
-start_x = 0
-stop_x = 600
-step_x = 150
+# Mock devices
+MOCK_SUNS = False
+MOCK_STAND = False
 
-start_y = 1800
-stop_y = 3600
-step_y = 10
+# 360 degrees = 128000 microsteps
+# 1 degree = ~ 355.56 microsteps
 
-itime = 38
-gain = 0
+# X-axis angle sweep settings in microsteps of motors
+START_X = 22044
+STOP_X = 24180
+STEP_X = 178
+
+# Y-axis angle sweep settings in microsteps of motors up to 360 degrees => 128000
+START_Y = 0
+STOP_Y = 128000
+STEP_Y = 160
+
+ITIME = 38
+GAIN = 0
 
 if __name__ == '__main__':
     log = open("SunS-log-" + str(int(time.time())) + ".csv", 'w')
 
-    if MOCK is False:
+    if MOCK_STAND is False:
         import serial
         serialStand = serial.Serial(
             port='COM6',
             baudrate=115200,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS
+            bytesize=serial.EIGHTBITS,
+            timeout=0.1
         )
     else:
         serialStand = Serial("stand")
 
+    if MOCK_SUNS is True:
+        ip = 'localhost'
+    else:
+        ip = '192.168.1.107'
+
     stand = Stand(serialStand)
-    suns = SunS(2, 0, '192.168.1.106', 23)
-    rcv = suns.connect()
-    print(rcv)
+    suns = SunS(ITIME, GAIN, ip, 23)
+    rcv_hello = suns.connect()
+    print(rcv_hello)
 
     print("Init! \r")
     time.sleep(1)
@@ -45,30 +59,32 @@ if __name__ == '__main__':
 
     stand.setXYReferencePosition()
     time.sleep(1)
-    print("Going back a little bit angle: " + str(-step_x/10.0) + " " + str(-step_y/10.0) + "\r")
-    stand.goToAPosition(-5, -5)
 
-    for x in range(start_x, stop_x+step_x, step_x):
-        for y in range(start_y, stop_y+step_y, step_y):
-            print("Setting angle: " + str(x/10.0) + " " + str(y/10.0) + "\r")
+    print("Going back a little bit angle: " + str(-STEP_X) + " " + str(-STEP_Y) + "\r")
+    stand.goToAPosition(-5000, -5000)
 
-            if y == 0:
-                stand.goToAPosition(x, 0)
-            else:
-                stand.goToAPosition(x, step_y)
+    #stand.goToAPosition(24888, 0)
+    #time.sleep(100)
 
-            stand.setYReferencePosition()
-            print("Done setting angle " + str(x/10.0) + " " + str(y/10.0) + "\r")
-            time.sleep(0.1)
+    for x in range(START_X, STOP_X+STEP_X, STEP_X):
+        print("Setting angle: " + str(stand.microstepsToDegrees(x)) + "\r")
+        stand.goToAPosition(x, 0)
+        for y in range(START_Y, STOP_Y+STEP_Y, STEP_Y):
+            print("Setting angle: " + str(stand.microstepsToDegrees(x)) + " " + str(stand.microstepsToDegrees(y)) + "\r")
+            stand.goToAPosition(x, y)
+            print("Done setting angle " + str(stand.microstepsToDegrees(x)) + " " + str(stand.microstepsToDegrees(y)) + "\r")
+            time.sleep(0.01)
 
+            # Trigger and receive value from the SunS
             SunS_result = suns.measure()
             print(SunS_result.strip())
-            log.writelines(str(time.strftime("%H:%M:%S")) + ";" + str(x) + ";" + str(y) + ";" + SunS_result.strip() + "\r")
+            log.writelines(str(time.strftime("%H:%M:%S")) + ";" + str(x) + ";" + str(y) + ";" + str(stand.microstepsToDegrees(x)) + ";" + str(stand.microstepsToDegrees(y)) + ";" + SunS_result.strip() + "\r")
             log.flush()
+        stand.setYReferencePosition()
     log.close()
+    suns.close()
 
     print("Moving back to start position!\r")
     stand.goToAPosition(0, 0)
 
     print("All done!\r")
-    suns.close()
